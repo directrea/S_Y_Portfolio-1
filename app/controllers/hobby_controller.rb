eb8c1b6266ff534534a66174be9167f90a5cb62c
shun_i <syunssssin@gmail.com>
2021-03-19 22:54:43 +0900

    base完成

---
+++ app/controllers/hobby_controller.rb
@@ -0,0 +1,7 @@
+class HobbyController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "main#index"
+  root "top#index"
+  get "/hobby" => "hobby#index"
+  get "/hobby/episode1" => "hobby#episode1"
+  get "/profile" => "profile#index"
 end
